27aca00100e67f87512083837122d5c78ece271a
sidee <said.issack@student.moringaschool.com>
2023-06-25 13:46:20 +0300

    completed assighnment

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 function animals(){
-    fetch('http;//localhost;3000/characters')
-    .then(Response => Response.json())
-    
-}
+    fetch('http://localhost:3000/characters')
+    .then(r => r.json())
+    .then(data => {console.log(data);})
+    .catch(error => {
+        console.error(error);
+      });
+  }
+animals()
---
+++ new js/conditionals.js
@@ -0,0 +1,7 @@
+if(condition)
+{//block of code to be executed
+}
+let numbermber = 10
+if(Number>0){
+    console.log("positive")
+}
