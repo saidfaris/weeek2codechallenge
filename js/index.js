f980201ae087e19fb8daaafb23d30e60126dd7c1
Sid Isaack <135071966+saidfaris@users.noreply.github.com>
2023-07-04 08:03:47 +0300

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function animals(){
-  fetch('http://localhost:3000/characters')
-  .then(r => r.json())
-  .then(data => {console.log(data);})
-  .catch(error => {
-      console.error(error);
-    });
-}
